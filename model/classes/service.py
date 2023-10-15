5b4f819f9464b1377b5477f7d8353d0b8e7f27f6
Sean McOwen <seanmcowen@gmail.com>
2023-10-15 15:40:58 -0400

    Add service

--- model/classes/service.py
+++ model/classes/service.py
@@ -1,4 +1,6 @@
 class Service:
 
-    def __init__(self):
-        pass
+    def __init__(self, name: str, portal_api_prefix: str, service_id: str):
+        self.name = name
+        self.portal_api_prefix = portal_api_prefix
+        self.service_id = service_id
