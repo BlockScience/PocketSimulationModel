a682b41f8176422b4687b78d342bdf4ff7539b0a
Sean McOwen <seanmcowen@gmail.com>
2023-10-16 00:28:29 -0400

    Add validator

--- model/classes/validator.py
+++ model/classes/validator.py
@@ -1,4 +1,15 @@
+from ..types import PublicKeyType, uPOKTType, ServiceURLType, StakeStatusType
+
 class Validator:
 
-    def __init__(self):
-        pass
+    def __init__(self, name: str, pokt_holdings: uPOKTType,
+                 staked_pokt: uPOKTType, service_url: ServiceURLType,
+                 operator_public_key: PublicKeyType, stake_status: StakeStatusType):
+        self.name = name
+        self.public_key = self
+        self.pokt_holdings = pokt_holdings
+        self.staked_pokt = staked_pokt
+        self.service_url = service_url
+        self.operator_public_key = operator_public_key
+        self.stake_status = stake_status
+
