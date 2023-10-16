6f15071a91267a3e14068ac72e3fba561aa78bba
Sean McOwen <seanmcowen@gmail.com>
2023-10-16 15:57:49 -0400

    Scaffold 2

--- model/psub/psub.py
+++ model/psub/psub.py
@@ -1,6 +1,6 @@
 from .meta import p_update_time, s_update_height, s_update_day
 
-
+# Block for recording things like time
 meta_update_block = {
     "policies": {
         "t": p_update_time,
@@ -8,5 +8,34 @@ meta_update_block = {
     "variables": {"day": s_update_day, "height": s_update_height},
 }
 
+# Block for anything joining the system
+join_block = {"policies": {}, "variables": {}}
 
-psub_blocks = [meta_update_block]
+# Portal delegation and service linking
+delegation_service_block = {"policies": {}, "variables": {}}
+
+# Relay requests
+relay_requests_block = {"policies": {}, "variables": {}}
+
+# Block for jailing or slashing behaviors
+jailing_slashing_block = {"policies": {}, "variables": {}}
+
+# Reward disbursement
+block_and_fee_rewards_block = {"policies": {}, "variables": {}}
+
+# Undelegation and service unlinking block
+undelegation_unservice_block = {"policies": {}, "variables": {}}
+
+# Any entities leaving the system
+leave_block = {"policies": {}, "variables": {}}
+
+psub_blocks = [
+    meta_update_block,
+    join_block,
+    delegation_service_block,
+    relay_requests_block,
+    jailing_slashing_block,
+    block_and_fee_rewards_block,
+    undelegation_unservice_block,
+    leave_block,
+]
