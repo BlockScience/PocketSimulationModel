02d57b8f3a823fe1fa4ceedb00fbe08a58e5c00b
Sean McOwen <seanmcowen@gmail.com>
2023-10-16 22:20:09 -0400

    Scaffold A

--- model/psub/psub.py
+++ model/psub/psub.py
@@ -1,8 +1,28 @@
 from .meta import p_update_time, s_update_height, s_update_day
-from .servicer import p_servicers_join, s_update_servicers
-from .service import p_service_linking, s_update_services, p_service_join
-from .portal import p_portal_join, s_update_portals
-from .application import p_application_join, s_update_applications
+from .servicer import (
+    p_servicers_join,
+    s_update_servicers,
+    p_relay_requests,
+    p_jailing_slashing,
+    p_servicers_leave,
+)
+from .service import (
+    p_service_linking,
+    s_update_services,
+    p_service_join,
+    p_service_unlinking,
+    p_service_leave,
+)
+from .portal import p_portal_join, s_update_portals, p_portal_leave
+from .application import (
+    p_application_join,
+    s_update_applications,
+    p_portal_delegation,
+    p_portal_undelegation,
+    p_application_leave,
+)
+from .treasury import p_block_reward, p_fee_reward, s_update_treasury
+from .validator import s_update_validators
 
 # Block for recording things like time
 meta_update_block = {
@@ -30,24 +50,80 @@ join_block = {
 
 # Portal delegation and service linking
 delegation_service_block = {
-    "policies": {"service_link": p_service_linking},
-    "variables": {"Servicers": s_update_servicers, "Services": s_update_services},
+    "policies": {
+        "service_link": p_service_linking,
+        "portal_delegation": p_portal_delegation,
+    },
+    "variables": {
+        "Servicers": s_update_servicers,
+        "Services": s_update_services,
+        "Portals": s_update_portals,
+        "Applications": s_update_applications,
+    },
 }
 
 # Relay requests
-relay_requests_block = {"policies": {}, "variables": {}}
+relay_requests_block = {
+    "policies": {"relay_requests": p_relay_requests},
+    "variables": {
+        "Servicers": s_update_servicers,
+        "Services": s_update_services,
+        "Portals": s_update_portals,
+        "Applications": s_update_applications,
+    },
+}
 
 # Block for jailing or slashing behaviors
-jailing_slashing_block = {"policies": {}, "variables": {}}
+jailing_slashing_block = {
+    "policies": {"jailing_slashing": p_jailing_slashing},
+    "variables": {
+        "Services": s_update_services,
+        "Portals": s_update_portals,
+    },
+}
 
 # Reward disbursement
-block_and_fee_rewards_block = {"policies": {}, "variables": {}}
+block_and_fee_rewards_block = {
+    "policies": {"block_reward": p_block_reward, "fee_reward": p_fee_reward},
+    "variables": {
+        "Treasury": s_update_treasury,
+        "Validators": s_update_validators,
+        "Servicers": s_update_servicers,
+        "Portals": s_update_portals,
+        "Applications": s_update_applications,
+    },
+}
 
 # Undelegation and service unlinking block
-undelegation_unservice_block = {"policies": {}, "variables": {}}
+undelegation_unservice_block = {
+    "policies": {
+        "service_unlink": p_service_unlinking,
+        "portal_undelegation": p_portal_undelegation,
+    },
+    "variables": {
+        "Servicers": s_update_servicers,
+        "Services": s_update_services,
+        "Portals": s_update_portals,
+        "Applications": s_update_applications,
+    },
+}
 
 # Any entities leaving the system
-leave_block = {"policies": {}, "variables": {}}
+leave_block = {
+    "policies": {
+        "servicer": p_servicers_leave,
+        "service": p_service_leave,
+        "portal": p_portal_leave,
+        "application": p_application_leave,
+    },
+    "variables": {
+        "Servicers": s_update_servicers,
+        "Services": s_update_services,
+        "Portals": s_update_portals,
+        "Applications": s_update_applications,
+    },
+}
+
 
 psub_blocks = [
     meta_update_block,
