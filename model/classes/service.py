7654d6ddf0a833d3f7a93b5cb4f771131ebd8330
Sean McOwen <seanmcowen@gmail.com>
2023-10-20 00:50:17 -0400

    Running

--- model/classes/service.py
+++ model/classes/service.py
@@ -1,5 +1,9 @@
+from typing import List
+from ..types import ServicerEntityType
+
 class Service:
-    def __init__(self, name: str, portal_api_prefix: str, service_id: str):
+    def __init__(self, name: str, portal_api_prefix: str, service_id: str, servicers: List[ServicerEntityType]):
         self.name = name
         self.portal_api_prefix = portal_api_prefix
         self.service_id = service_id
+        self.servicers = []
