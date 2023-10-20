555f7a228735a044e6a8d280358285c3314b970f
Sean McOwen <seanmcowen@gmail.com>
2023-10-19 21:21:28 -0400

    portal_join_policy

--- model/boundary_actions/__init__.py
+++ model/boundary_actions/__init__.py
@@ -1,3 +1,4 @@
 from .application import application_join_ba
 from .portal import portal_join_ba
 from .servicer import servicer_join_ba
+from .service import service_join_ba
---
+++ model/policy/portal.py
@@ -0,0 +1,23 @@
+from ..types import StateType, ParamType
+from ..spaces import portal_join_space, portal_entity_space
+from typing import Tuple
+from ..classes import portal
+
+
+def portal_join_policy(
+    state: StateType, params: ParamType, domain: Tuple[portal_join_space]
+) -> Tuple[portal_entity_space]:
+    space: portal_join_space = domain[0]
+    # Create entity
+    portal = portal(
+        name=space["name"],
+        pokt_holdings=space["personal_holdings"],
+        staked_pokt=space["stake_amount"],
+        services=[],
+        geo_zone=space["geo_zone"],
+        number_of_services=space["number_servicers"],
+        stake_status="Staked",
+        unstaking_height=None,
+        delegate=None,
+    )
+    return ({"portal": portal},)
