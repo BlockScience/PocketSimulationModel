fe05dfe4253a223efed84c631e4bd91a53231da8
Sean McOwen <seanmcowen@gmail.com>
2023-12-07 16:09:32 -0500

    Add pickle writing

--- cloud_run.py
+++ cloud_run.py
@@ -1,4 +1,10 @@
 from model import run_experiments
+import pickle
 
-df = run_experiments(["Base"])
-print(df)
+experiment_name = "Base"
+df = run_experiments([experiment_name])
+
+file_name = open("data/{}.pkl".format(experiment_name), "ab")
+pickle.dump(df, file_name)
+file_name.close()
+print("Complete!")
