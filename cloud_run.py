990d0fea8ff462dd49c4392ea7758199107bce51
Sean McOwen <seanmcowen@gmail.com>
2023-12-08 19:58:38 -0500

    Boto

--- cloud_run.py
+++ cloud_run.py
@@ -1,6 +1,5 @@
 import os
-
-print(os.listdir("."))
+import boto3
 
 from model import run_experiments
 import pickle
@@ -9,7 +8,15 @@ import sys
 experiments = sys.argv[1:]
 df = run_experiments(experiments)
 
+
 file_name = open("data/{}.pkl".format("-".join(experiments)), "ab")
 pickle.dump(df, file_name)
 file_name.close()
 print("Complete!")
+s3 = boto3.resource("s3")
+s3.meta.client.upload_file(
+    "data/{}.pkl".format("-".join(experiments)),
+    "pocketsimulation",
+    "data/{}.pkl".format("-".join(experiments)),
+)
+print("Uploaded!")
