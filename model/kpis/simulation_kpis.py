9a9d47a27c8796047ded25fd9c1b2f19693502ca
Sean McOwen <seanmcowen@gmail.com>
2023-12-13 15:51:20 -0500

    KPI 11

--- model/kpis/simulation_kpis.py
+++ model/kpis/simulation_kpis.py
@@ -55,17 +55,41 @@ def compute_kpi_e(unique_servicers):
     return pd.Series(kpi_e)
 
 
+def compute_kpi_11(unique_servicers):
+    # Add in KPI 11
+    kpi_11 = {}
+    for key in unique_servicers:
+        servicers = unique_servicers[key]
+        for servicer in servicers.values():
+            servicer.kpi_11 = (
+                sum(servicer.jail_lost_revenue_history.values())
+                / servicer.total_revenues
+            )
+        temp = [x.kpi_11 for x in servicers.values()]
+        if len(temp) > 0:
+            kpi_11[key] = sum(temp) / len(temp)
+        else:
+            kpi_11[key] = None
+    return pd.Series(kpi_11)
+
+
 def compute_kpi_14(unique_servicers):
     # Add in KPI 14, average slashing
     kpi_14 = {}
     for key in unique_servicers:
         servicers = unique_servicers[key]
         for servicer in servicers.values():
-            servicer.kpi_14 = (
-                servicer.staked_pokt_total_inflow / servicer.total_revenues
-            )
-        temp = [x.kpi_14 for x in servicers.values()]
-        kpi_14[key] = sum(temp) / len(temp)
+            if servicer.total_revenues > 0:
+                servicer.kpi_14 = (
+                    servicer.staked_pokt_total_inflow / servicer.total_revenues
+                )
+            else:
+                servicer.kpi_14 = None
+        temp = [x.kpi_14 for x in servicers.values() if x.kpi_14]
+        if len(temp) > 0:
+            kpi_14[key] = sum(temp) / len(temp)
+        else:
+            kpi_14[key] = None
     return pd.Series(kpi_14)
 
 
@@ -102,6 +126,7 @@ def create_simulation_kpis(df):
     simulation_kpis = simulation_kpis.join(params)
     # simulation_kpis["KPI 8"] = compute_kpi8(unique_servicers)
     simulation_kpis["KPI E"] = compute_kpi_e(unique_servicers)
+    simulation_kpis["KPI 11"] = compute_kpi_11(unique_servicers)
     simulation_kpis["KPI 14"] = compute_kpi_14(unique_servicers)
 
     return simulation_kpis
