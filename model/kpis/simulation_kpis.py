ed92671d8ba631de3893d8dd815148ff5c285de3
Sean McOwen <seanmcowen@gmail.com>
2023-12-13 15:26:05 -0500

    KPI 14

--- model/kpis/simulation_kpis.py
+++ model/kpis/simulation_kpis.py
@@ -52,7 +52,21 @@ def compute_kpi_e(unique_servicers):
             ]
         )
         kpi_e[key] = np.exp(-slashing_cost / jailing_cost)
-        return pd.Series(kpi_e)
+    return pd.Series(kpi_e)
+
+
+def compute_kpi_14(unique_servicers):
+    # Add in KPI 14, average slashing
+    kpi_14 = {}
+    for key in unique_servicers:
+        servicers = unique_servicers[key]
+        for servicer in servicers:
+            servicer.kpi_14 = (
+                servicer.staked_pokt_total_inflow / servicer.total_revenues
+            )
+        temp = [x.kpi_14 for x in servicers]
+        kpi_14[key] = sum(temp) / len(temp)
+    return pd.Series(kpi_14)
 
 
 def create_simulation_kpis(df):
@@ -88,4 +102,6 @@ def create_simulation_kpis(df):
     simulation_kpis = simulation_kpis.join(params)
     # simulation_kpis["KPI 8"] = compute_kpi8(unique_servicers)
     simulation_kpis["KPI E"] = compute_kpi_e(unique_servicers)
+    simulation_kpis["KPI 14"] = compute_kpi_14(unique_servicers)
+
     return simulation_kpis
