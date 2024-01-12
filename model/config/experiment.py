ee2cf1e8381e4ef74919431c244890a8dc063fda
eightarmsninebrains <144281779+eightarmsninebrains@users.noreply.github.com>
2024-01-12 09:21:32 -0500

    Updating experiment.py with new method
    The method adds experiments to the global experimental_setups dict based on a specificed set of parameter-describing strings.

--- model/config/experiment.py
+++ model/config/experiment.py
@@ -1,3 +1,5 @@
+from typing import List
+
 experimental_setups = {
     "test1": {
         "config_option_state": "Test",
@@ -17,19 +19,15 @@ experimental_setups = {
         "monte_carlo_n": 1,
         "T": 365,
     },
-    "BaseEvent": {
-        "config_option_state": "Base",
-        "config_option_params": "BaseEvent",
-        "monte_carlo_n": 1,
-        "T": 365,
-    },
 }
+
+
 
 for i in range(1, 28):
     experimental_setups["Test{}".format(i)] = {
         "config_option_state": "Base",
         "config_option_params": "Test{}".format(i),
-        "monte_carlo_n": 30,
+        "monte_carlo_n": 1,
         "T": 365,
     }
 
@@ -37,38 +35,25 @@ for i in range(1, 289):
     experimental_setups["gateway_viability_sweep_ag1_{}".format(i)] = {
         "config_option_state": "Base",
         "config_option_params": "gateway_viability_sweep_ag1_{}".format(i),
-        "monte_carlo_n": 2,
+        "monte_carlo_n": 1,
         "T": 365,
     }
 
-for i in range(1, 49):
-    experimental_setups["network_failures_service_ag1_{}".format(i)] = {
-        "config_option_state": "Base",
-        "config_option_params": "network_failures_service_ag1_{}".format(i),
-        "monte_carlo_n": 2,
-        "T": 365,
-    }
+def add_experiments_from_param_strings(param_strings: List[str],
+                                      monte_carlo_n: int,
+                                      T: int,
+                                      experiment_dict = experimental_setups) -> None:
+    """
+    Given a list of strings corresponding to parameters,
+    add an experimental setup for each one. 
+    """
+    for param_string in param_strings:
+        experiment_dict[param_string] = {
+            "config_option_state": "Base",
+            "config_option_params": param_string, 
+            "monte_carlo_n": monte_carlo_n,
+            "T": T 
+        }
+    return None #Used for side effect of adding to experimental_setups. 
 
-for i in range(1, 1153):
-    experimental_setups["servicer_viability_ag1_{}".format(i)] = {
-        "config_option_state": "Base",
-        "config_option_params": "servicer_viability_ag1_{}".format(i),
-        "monte_carlo_n": 2,
-        "T": 365,
-    }
 
-for i in range(1, 1153):
-    experimental_setups["network_viability_ag1_{}".format(i)] = {
-        "config_option_state": "Base",
-        "config_option_params": "network_viability_ag1_{}".format(i),
-        "monte_carlo_n": 2,
-        "T": 365,
-    }
-
-for i in range(1, 2689):
-    experimental_setups["network_failures_oracle_ag1_{}".format(i)] = {
-        "config_option_state": "Base",
-        "config_option_params": "network_failures_oracle_ag1_{}".format(i),
-        "monte_carlo_n": 2,
-        "T": 365,
-    }
