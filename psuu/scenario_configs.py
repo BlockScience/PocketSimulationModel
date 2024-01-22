07348e6cae390300c2518b8556e059797def4134
SeanMcOwen <seanmcowen@gmail.com>
2024-01-22 16:34:56 -0500

    network_failures_oracle_ag scenario config

--- psuu/scenario_configs.py
+++ psuu/scenario_configs.py
@@ -21,3 +21,34 @@ scenario_configs["gateway_viability_sweep_ag"] = {
         "t2": 0.9,
     },
 }
+scenario_configs["network_failures_oracle_ag"] = {
+    "variable_params": [
+        "relays_to_tokens_multiplier",
+        "gateway_fee_per_relays",
+        "application_fee_per_relay",
+        "gateway_minimum_stake",
+        "minimum_application_stake",
+        "dao_allocation",
+        "validator_fee_percentage",
+    ],
+    "control_params": ["oracle_treatment_time_mean", "event"],
+    "threshold_inequalities": [
+        "servicer_npv",
+        "gateway_npv",
+        "circulating_supply_available_supply_ratio",
+        "net_inflation",
+        "dao_value_capture",
+    ],
+    "threshold_parameters": {
+        "s1": 750 * 1e6,
+        "s2": 0.9,
+        "t1": 7500 * 1e6,
+        "t2": 0.9,
+        "v1": 0.3 * 1e6,
+        "y1": -0.1,
+        "y2": 0.05,
+        "z1": 0.02,
+        "z2": 0.1,
+        "z3": 0.9,
+    },
+}
