f93513b2b4f00dd402745116e3bc68c93677add5
SeanMcOwen <seanmcowen@gmail.com>
2024-01-24 14:42:08 -0500

    Almost pieced together

--- psuu/meta_programming.py
+++ psuu/meta_programming.py
@@ -1,10 +1,10 @@
 def build_next_param_config_code(
-    next_name, new_param_grid, control_params, param_config
+    next_name, new_param_grid, variable_params, control_params, param_config
 ):
     print("Add the following to model/config/params.py:")
     print()
     print('{} = build_params("Base")'.format(next_name))
-    for x in new_param_grid:
+    for x in variable_params:
         print('{}["{}"] = {}'.format(next_name, x, new_param_grid[x]))
     for x in control_params:
         print('{}["{}"] = {}'.format(next_name, x, param_config[x]))
